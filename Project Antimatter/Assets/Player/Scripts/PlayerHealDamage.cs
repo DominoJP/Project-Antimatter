85a572382809ae0fd8ec0bdc84b17cb0374825a6
AzureM0on <adrian.vega935@gmail.com>
2022-11-14 19:15:25 -0800

    Added Enemy patrol & chase + health script
    problems:
    need to fit health bar
    enemy stops once chase is triggered

--- Project Antimatter/Assets/Player/Scripts/PlayerHealDamage.cs
+++ Project Antimatter/Assets/Player/Scripts/PlayerHealDamage.cs
@@ -7,15 +7,19 @@ public class PlayerHealDamage : MonoBehaviour
     public int health;
     public int maxHealth = 10;
 
+    public HealthBar healthBar;
+
     // Start is called before the first frame update
     void Start()
     {
         health = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     public void TakeDamage(int amount)
     {
         health -= amount;
+        healthBar.SetHealth(health);
         if(health <= 0)
         {
             Destroy(gameObject);

28b1835c11d68133d834945ec281c9193363735f
DominoJP <joshjplano@gmail.com>
2022-11-15 07:13:33 -0800

    Boss animations and scene changes
    Added boss animations and enemy death explosion. Additionally scene changes

---
+++ Project Antimatter/Assets/Enemy/Enemy1/Scripts/TankAI.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TankAI : MonoBehaviour
+{
+    public float Speed;
+    public float StoppingDistance;
+    private Transform Target;
+    public GameObject EnemyExplosion;
+    public Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, Target.position) > StoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Target.position, Speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D()
+    {
+
+        animator.Play("EnemyExplosion");
+            Destroy(gameObject);
+      
+    }
+
+}
